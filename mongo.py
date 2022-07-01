1d365c6dfd59ad3ae8bdae7226e7b6ff10c0333f
Aryash <aryashahvar14@gmail.com>
2022-07-01 14:06:12 -0400

    Getting the requests

--- mongo.py
+++ mongo.py
@@ -8,12 +8,12 @@ def get_database():
 
     # Provide the mongodb atlas url to connect python to mongodb using pymongo
     # username = os.environ.get('mongoDb')
-    f = open("monogDBURI.txt", "r")
-    CONNECTION_STRING = f.read()
-    f.close()
+    # f = open("monogDBURI.txt", "r")
+    # CONNECTION_STRING = f.read()
+    # f.close()
 
     # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-    client = MongoClient(CONNECTION_STRING)
+    client = MongoClient("mongodb+srv://Adm:instutor@cluster0.s0gxd.mongodb.net/?retryWrites=true&w=majority")
 
     # Create the database for our example (we will use the same database throughout the tutorial
     return client['deployment']
@@ -23,20 +23,22 @@ if __name__ == "__main__":
     # Get the database
     dbname = get_database()
     print(dbname.list_collection_names())
+    # requests
+    cursor = dbname['requests'].find()
+    # profiles
     cursor = dbname['profiles'].find()
-    # with open('output.json', 'w') as outfile:
-    #     for document in cursor:
-    #         json.dumps(document, outfile)
+
     with open('output.json', 'w') as outfile:
         for document in cursor:
-            # for k, v in document.items():
-            #     if (k == "_id" or k == "user"):
-            #         document[k] = str(v)
-            #     # print(k, v)
+            #for requests:
             document["_id"] = str(document["_id"])
             document["user"] = str(document["user"])
-            for item in document["expertise"]:
-                item["_id"] = str(item["_id"])
-            document["date"] = str(document["date"])
+            document["last_edit_time"] = str(document["last_edit_time"])
 
+            #for profiles:
+            # document["_id"] = str(document["_id"])
+            # document["user"] = str(document["user"])
+            # for item in document["expertise"]:
+            #     item["_id"] = str(item["_id"])
+            # document["date"] = str(document["date"])
             json.dump(document, outfile, indent = 2)
